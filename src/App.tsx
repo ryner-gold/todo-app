92e191ea5b5d405155aff0358b0544fcccee2741
Ryner <individualog@gmail.com>
2025-03-26 23:09:32 +0000

    feat: create task form skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,21 @@ function App() {
 
                 <div className="column__right">
                     <h2>Create Task</h2>
-                   
+
+                    <div className="form__container">
+                        <form className="form">
+                            <label className="form__title"> Title:
+                                <input name="task-title" id="task-title" placeholder="Input task title"/>
+                            </label>
+                            <label>
+                                Description:
+                                <input name="task-description" id="task-description"
+                                       placeholder="Input task description"/>
+                            </label>
+                            <button type="submit">Search</button>
+                        </form>
+                    </div>
+
                 </div>
             </main>
         </div>
